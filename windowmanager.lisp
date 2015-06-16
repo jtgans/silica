67a4b17650bcfbbac7b6cf2b37b7b3c1d098a39d
June Tate-Gans <june@theonelab.com>
2015-06-16 10:59:43 -0700

    Moving the old gypsum window manager code into silica.

--- windowmanager.lisp
+++ windowmanager.lisp
@@ -1,4 +1,4 @@
-(in-package :silica)
+(in-package :cl-gypsum-client)
 
 (defvar *screen-width* 640
   "The screen width in pixels.")
@@ -6,9 +6,49 @@
 (defvar *screen-height* 360
   "The screen height in pixels.")
 
-(defclass window-manager (handler)
-  (:documentation "An application is simply a handler."))
+(defvar *gap-width 3
+  "The size of the frame gaps in pixels.")
 
-(defvar *window-manager*
-  (make-instance 'application
-                 :name "Window Manager"))
+(defclass frame (view)
+  ((direction    :initform nil :accessor frame-division :type symbol)
+   (ratio        :initform 1/2 :accessor frame-ratio    :type ratio)
+   (first-child  :initform nil :accessor frame-first    :initarg first  :type (or frame null))
+   (second-child :initform nil :accessor frame-second   :initarg second :type (or frame null)))
+  (:documentation "A frame is a VIEW that represents a tree of other VIEWs and
+is used to manage application windows on screen."))
+
+(defvar *frame-tree* (make-instance 'frame
+                                    :rect (make-rect :width *screen-width* :height *screen-height*))
+  "The region layout BSP tree. Each element of the tree is a frame struct.")
+
+(defun frame-leaf-p (frame)
+  "Determines if the given FRAME is a leaf node."
+  (and (null (frame-first-child frame))
+       (null (frame-second-child frame))))
+
+(defun split-frame-leaf (frame direction &key (ratio 1/2) (gutter 1))
+  "Splits a FRAME in two in the DIRECTION (:horizontal or :vertical),
+creating two additional children with the given split RATIO."
+  (unless (frame-leaf-p frame) (error "FRAME must be a leaf node."))
+  (with-slots (rect first-child second-child parent-view child-views) frame
+    (multiple-value-bind (first second)
+        (subdivide-rect rect direction :ratio ratio :gutter gutter)
+      ;; Set the parent's children to the new frames
+      (setf first-child first
+            second-child second)
+
+      ;; Set the split direction and ratio for the parent
+      (setf (frame-direction frame) direction
+            (frame-ratio frame) ratio)
+
+      ;; Move the child views to the new child
+      (setf (view-children frame) nil
+            (view-children first) child-views)
+
+      ;; Set the parent on the new frames
+      (post-event first-child (make-instance 'assign-event :frame frame))
+      (post-event second-child (make-instance 'assign-event :frame frame))
+
+      ;; Send the assign event to the children
+      (loop for child in (view-children first) do
+           (post-event child (make-instance 'assign-event :frame first))))))
