bcf85aa1c3c3803bdcada7ef691dbf1e44184511
June Tate-Gans <june@theonelab.com>
2015-06-16 09:55:31 -0700

    Moving event and handler into cl-event-handler.

--- event.lisp
+++ event.lisp
@@ -1,24 +1,5 @@
 (in-package :silica)
 
-(defclass event ()
-  ((sender :initarg :sender
-           :type handler
-           :reader sender))
-  (:documentation "An event. SENDER contains the HANDLER to use for
-responses."))
-
-(defclass ping-event (event)
-  ()
-  (:documentation "An event used to ping a handler for aliveness."))
-
-(defclass pong-event (event)
-  ()
-  (:documentation "An event used as a reply to PING-EVENT events."))
-
-(defclass quit-event (event)
-  ()
-  (:documentation "An event used to ask a handler to stop running."))
-
 (defclass draw-event (event)
   ()
   (:documentation "An event that represents a draw request."))
