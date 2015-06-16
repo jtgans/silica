d8597b811a7ab02488145ad4919823bb0ad8c700
June Tate-Gans <june@theonelab.com>
2015-06-15 22:18:34 -0700

    Adding in some default vars for screen dimensions.

--- silica.lisp
+++ silica.lisp
@@ -4,5 +4,11 @@
 
 (export 'silica)
 
+(defvar *screen-width* 640
+  "The width of the Gypsum screen.")
+
+(defvar *screen-height* 360
+  "The height of the Gypsum screen.")
+
 (defun silica ()
   (format t "Silica v0.1~%"))
