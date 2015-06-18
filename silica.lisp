fee44ca6d1f19d88295adf07aa5e1856a21cd076
June Tate-Gans <june@theonelab.com>
2015-06-17 22:12:39 -0700

    Reworing silica.
    We now have a system-handler for handling generic... events. Or
    something. It also spins up the whole shebang by bringing up the window
    manager, input event manager, and gypsum babysitter.

--- silica.lisp
+++ silica.lisp
@@ -2,13 +2,28 @@
 
 (in-package #:silica)
 
-(export 'silica)
+(defclass system-handler (handler)
+  ()
+  (:documentation "This handler represents the Silica system event handler. It's
+used to bring up and shutdown the system. Effectively, all this handler does is
+wait for a QUIT-EVENT and then terminates the system."))
 
-(defvar *screen-width* 640
-  "The width of the Gypsum screen.")
+(defmethod start-handler ((handler system-handler))
+  "Starts the SYSTEM-HANDLER in a synchronous way to prevent exits from
+occurring before we're ready."
+  (with-slots (name event-channel event-task) handler
+    (log:info "System handler starting, waiting for events.")
+    (handler-runloop handler)))
 
-(defvar *screen-height* 360
-  "The height of the Gypsum screen.")
+(defvar *system-handler* (make-instance 'system-handler :start-immediately nil)
+  "The one reference to the single SYSTEM-HANDLER instance in the system.")
 
-(defun silica ()
-  (format t "Silica v0.1~%"))
+(defun start-silica ()
+  "Starts the system. Effectively just starts any of the handlers requested at
+startup, connects to Gypsum, and starts the system event handler."
+  (log:info "Silica v0.1~%")
+  (pcall #'read-event-loop)
+  (start-handler *display-manager*)
+  (start-handler *system-handler*))
+
+(export '(start-silica))
