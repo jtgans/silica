38ae51a30db5c5086e46fb40490b928c7d169aeb
June Tate-Gans <june@theonelab.com>
2016-02-23 21:53:07 -0600

    Adding in a display manager to help manage the connection to Gypsum.

---
+++ displaymanager.lisp
@@ -0,0 +1,16 @@
+(in-package :silica)
+
+(defclass display-manager (handler)
+  ()
+  (:documentation "The DISPLAY-MANAGER, like the WINDOW-MANAGER and
+INPUT-MANAGER is a singleton class that ensures that the connection to a Gypsum
+display is active and running."))
+
+(defmethod start-handler ((handler display-manager))
+  (connect :tcp))
+
+(defmethod handle-event ((handler display-manager) (event lost-connection-event))
+  (with-mutex (cl-gypsum-client:*write-lock*)
+    ))
+
+(defvar *display-manager* (make-instance 'display-manager :start-immediately nil))
