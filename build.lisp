940c0e566d7e3a43a5a189f31d1367cb0da6c115
June Tate-Gans <june@theonelab.com>
2016-02-23 21:52:22 -0600

    Updating the entry point.

--- build.lisp
+++ build.lisp
@@ -5,6 +5,6 @@
 (sb-ext:save-lisp-and-die "../out/silica" :toplevel
                           (lambda ()
                             (sb-posix:putenv (format nil "SBCL_HOME=~A" #.(sb-ext:posix-getenv "SBCL_HOME")))
-                            (silica:silica)
+                            (silica:start-silica)
                             0)
                           :executable t)
